357d54e32db8029d8385b8f49271d63e0d633175
Jason_xy <1462357003@qq.com>
2021-07-29 10:37:25 +0800

    Quick calibration

---
+++ ANO/APP/MoveControl.h
@@ -0,0 +1,21 @@
+#ifndef __MOVECONTROL_H__
+#define __MOVECONTROL_H__
+#include "SysConfig.h"
+#include "LX_FC_Fun.h"
+#include "ANO_DT_LX.h"
+#include "LX_FC_State.h"
+#include "Ano_math.h"
+
+//MoveControl Get OneByte from USART2
+void MoveControl_GetOneByte(uint8_t data);
+void MoveControl_DataAnl(uint8_t *data, uint8_t len);
+void MoveControl_Output(void);
+uint8_t MoveControl_Spin(uint16_t spin_angle_0_360, uint16_t spin_speed_dps);
+
+void OVControl_GetOneByte(uint8_t data);
+void OVControl_DataAnl(uint8_t *data, uint8_t len);
+
+//Quick calibration
+uint8_t Quick_Calibration(void);
+
+#endif //__MOVECONTROL_H__
