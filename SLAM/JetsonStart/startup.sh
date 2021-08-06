e330781636f0b608f24bd7a931d97bfeaf5826f5
jason-xy <1462357003@qq.com>
2021-08-06 15:58:42 +0800

    JetsonStart

--- SLAM/JetsonStart/startup.sh
+++ SLAM/JetsonStart/startup.sh
@@ -1,9 +1,16 @@
-#!/bin/bash 
-cd ~/catkin_ws
-source ~/.bashrc
-source devel/setup.bash
+#! /bin/bash
+#刷新ros环境
 source /opt/ros/melodic/setup.bash
-gnome-terminal -- bash -c "cd ~/catkin_ws;source ~/.bashrc;source devel/setup.bash;source /opt/ros/melodic/setup.bash;roscore"
+# 刷新功能包环境
+source /home/hcm/catkin_ws/devel/setup.bash
+# 运行相机节点
+nohup roslaunch realsense2_camera rs_t265.launch enable_fisheye1:=true enable_fisheye2:=true &
 sleep 5
-gnome-terminal -- bash -c "cd ~/catkin_ws;source ~/.bashrc;source /opt/ros/melodic/setup.bash;source devel/setup.bash;roslaunch realsense2_camera rs_t265.launch"
-gnome-terminal -- bash -c "cd ~/catkin_ws;source ~/.bashrc;source /opt/ros/melodic/setup.bash;source devel/setup.bash;rosrun slampose slampose"
+
+#运行数据节点
+nohup rosrun slampose slampose &
+# nohup rosrun showpath showpath &
+sleep 10
+cd /home/hcm/catkin_ws/bag
+# nohup rosbag record -a &
+
