c75f8f6f20098906663ae300eccf72078ae5d8a5
Jason_xy <1462357003@qq.com>
2021-10-22 17:25:05 +0800

    启动文件update

--- SLAM/JetsonStart/startup.sh
+++ SLAM/JetsonStart/startup.sh
@@ -2,15 +2,15 @@
 #刷新ros环境
 source /opt/ros/melodic/setup.bash
 # 刷新功能包环境
-source /home/hcm/catkin_ws/devel/setup.bash
+source /home/jason/catkin_ws/devel/setup.bash
+# 启动ROS
+nohup roscore &
+sleep 10
 # 运行相机节点
 nohup roslaunch realsense2_camera rs_t265.launch enable_fisheye1:=true enable_fisheye2:=true &
-sleep 5
-
 #运行数据节点
 nohup rosrun slampose slampose &
-# nohup rosrun showpath showpath &
-sleep 10
+nohup rosrun showpath showpath &
+# sleep 10
 # cd /home/hcm/catkin_ws/bag
 # nohup rosbag record -a &
-
