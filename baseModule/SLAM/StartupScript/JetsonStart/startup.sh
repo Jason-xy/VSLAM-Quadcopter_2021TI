86510e9ea1b468d695bca056a1344851cf09ebe9
Jason_xy <1462357003@qq.com>
2021-11-04 16:53:04 +0800

    update startup script

--- baseModule/SLAM/StartupScript/JetsonStart/startup.sh
+++ baseModule/SLAM/StartupScript/JetsonStart/startup.sh
@@ -7,10 +7,18 @@ source /home/jason/catkin_ws/devel/setup.bash
 nohup roscore &
 sleep 10
 # 运行相机节点
-nohup roslaunch realsense2_camera rs_t265.launch enable_fisheye1:=true enable_fisheye2:=true &
-#运行数据节点
+nohup roslaunch realsense2_camera rs_t265.launch &
+# 运行数据节点
 nohup rosrun slampose slampose &
+# 显示运动路径
 nohup rosrun showpath showpath &
+# 运行视觉节点
+nohup python /home/jason/catkin_ws/src/cvtask/scripts/talker.py &
+# 运行控制节点
+nohup rosrun control control &
+# wifi
+nohup sudo usb_modeswitch -KW -v 0bda -p 1a2b &
 # sleep 10
 # cd /home/hcm/catkin_ws/bag
 # nohup rosbag record -a &
+
